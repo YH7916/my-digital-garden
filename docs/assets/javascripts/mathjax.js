61921991e033b4f6466456f2b600507a9b91adef
YH7916 <3188127343@qq.com>
2026-06-09 09:50:31 +0800

    Fix MathJax rendering for physics formulas

--- docs/assets/javascripts/mathjax.js
+++ docs/assets/javascripts/mathjax.js
@@ -1,15 +1,32 @@
 window.MathJax = {
-tex: {
- inlineMath: [["$", "$"], ["\\(", "\\)"]],
- displayMath: [["$$", "$$"], ["\\[", "\\]"]],
- processEscapes: true,
- processEnvironments: true
-},
-options: {
- ignoreHtmlClass: ".*|",
- processHtmlClass: "arithmatex"
-}
+  loader: {
+    load: ["[tex]/boldsymbol", "[tex]/physics", "[tex]/unicode"]
+  },
+  tex: {
+    packages: { "[+]": ["boldsymbol", "physics", "unicode"] },
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true,
+    macros: {
+      bm: ["\\boldsymbol{#1}", 1],
+      oiint: "\\mathop{\\unicode{x222F}}\\nolimits",
+      oiiint: "\\mathop{\\unicode{x2230}}\\nolimits"
+    }
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  },
+  startup: {
+    typeset: false,
+    ready: () => {
+      MathJax.startup.defaultReady();
+      document$.subscribe(() => {
+        const nodes = [...document.querySelectorAll(".arithmatex")]
+          .filter(node => !node.querySelector("mjx-container"));
+        MathJax.typesetPromise(nodes);
+      });
+    }
+  }
 };
-document$.subscribe(() => { 
-MathJax.typesetPromise()
-})
